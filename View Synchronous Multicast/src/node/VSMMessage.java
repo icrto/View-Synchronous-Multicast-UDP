4b6761c38da0e71b027865f93bb502379bc79909
danielgranhao <up201406280@fe.up.pt>
2018-12-14 11:48:32 +0000

    Added fields to VSMMessage and View

--- View Synchronous Multicast/src/node/VSMMessage.java
+++ View Synchronous Multicast/src/node/VSMMessage.java
@@ -1,5 +1,60 @@
 package node;
 
-public class VSMMessage {
+import java.io.Serializable;
+
+public class VSMMessage implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1295987829600477791L;
+	
+	private int viewId;
+	
+	private int senderId;
+	
+	private int seqN;
+	
+	private String payload;
+
+	public VSMMessage(int viewId, int senderId, int seqN, String payload) {
+		super();
+		this.viewId = viewId;
+		this.senderId = senderId;
+		this.seqN = seqN;
+		this.payload = payload;
+	}
+
+	public int getViewId() {
+		return viewId;
+	}
+
+	public void setViewId(int viewId) {
+		this.viewId = viewId;
+	}
+
+	public int getSenderId() {
+		return senderId;
+	}
+
+	public void setSenderId(int senderId) {
+		this.senderId = senderId;
+	}
+
+	public int getSeqN() {
+		return seqN;
+	}
+
+	public void setSeqN(int seqN) {
+		this.seqN = seqN;
+	}
+
+	public String getPayload() {
+		return payload;
+	}
+
+	public void setPayload(String payload) {
+		this.payload = payload;
+	}
 
 }
--- View Synchronous Multicast/src/node/View.java
+++ View Synchronous Multicast/src/node/View.java
@@ -1,5 +1,32 @@
 package node;
 
-public class View {
+import java.util.ArrayList;
 
+public class View {
+	
+	private int id;
+	
+	private ArrayList<Integer> nodes;
+
+	public View(int id, ArrayList<Integer> nodes) {
+		super();
+		this.id = id;
+		this.nodes = nodes;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ArrayList<Integer> getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(ArrayList<Integer> nodes) {
+		this.nodes = nodes;
+	}
 }
