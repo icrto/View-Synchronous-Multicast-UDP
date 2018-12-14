74740b34d0f905df86bf930b73e308e359c7b6ab
danielgranhao <up201406280@fe.up.pt>
2018-12-14 12:47:08 +0000

    Added some comments on next steps to take in VSM development

--- View Synchronous Multicast/src/node/VSM.java
+++ View Synchronous Multicast/src/node/VSM.java
@@ -4,9 +4,22 @@ public class VSM {
 	
 	public void sendVSC() {
 		
+		/* TODO: 
+		 * 1º - Espera até terminar mudança de vista caso esteja a meio da mesma
+		 * 2º - Enviar mensagem em multicast udp
+		 * 3º - Correr recv para esta mensagem devido à propriedade de self-delivery
+		*/
+		
 	}
 	
 	public String recvVSC() {
+		
+		/* TODO:
+		 * 1º - Chamar receive da Network Emulation - é bloqueante até receber alguma coisa
+		 * 2º - Enviar ack relativo a esta mensagem em multicast UDP
+		 * 3º - Esperar
+		*/
+		
 		return null;
 	}
 }
