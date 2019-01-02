cb52f244160641f69914f3271a4cfee289f5998f
icrto <up201403560@fe.up.pt>
2019-01-02 17:58:28 +0000

    TCP working - added reset

--- ControllerApp/src/controller/Controller.java
+++ ControllerApp/src/controller/Controller.java
@@ -1,63 +1,74 @@
 package controller;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import view.View;
 
-import java.net.InetAddress;
-
 public class Controller {
-	private static DatagramSocket publisherSocket;
-	private static DatagramPacket sendPacket;
-	private static InetAddress IPAddress;
 	private static View currentView;
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		publisherSocket = new DatagramSocket();
-		IPAddress = InetAddress.getByName("localhost");
+	private static Integer nrNodes;
+	private static int basePort = 60000;
+	private static Socket item;
+	private static ArrayList<Socket> sockets = new ArrayList<Socket>();
+	private static ArrayList<ObjectOutputStream> outputStreams = new ArrayList<ObjectOutputStream>();
+	
+	public static void main(String[] args){
+		if(args.length < 1) {
+			System.out.println("Usage:");
+			System.out.println("java -jar controller.jar <number of nodes>");
+			System.exit(-1);
+		}
+        nrNodes = Integer.parseInt(args[0]);
+
+        //create sockets
+		for(int i = 0; i < nrNodes; i++) {
+			try {
+				item = new Socket("localhost", basePort + i);
+				sockets.add(item);
+				outputStreams.add(new ObjectOutputStream(item.getOutputStream()));
+			} catch (IOException e) {
+				System.err.println("Could Not Listen on Port: " + (basePort + i));
+				System.exit(-1);
+			}
+			
+		}
+		
 		currentView = new View();
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		String line;
 		String[] items;
-		boolean changeView = false;
 
 		System.out.println("Controller App Started");
 		System.out.println("Usage:");
 		System.out.println("join <node id1> <node id2> ... <node idn>");
 		System.out.println("leave <node id> <node id2> ... <node idn>");
 		System.out.println(currentView.toString());
+		//loop to read command line input
 		while(true) {
 			line = scan.nextLine();
 
 			if(line.indexOf("join") != -1) {
 				items = line.split(" ");
 				for(int i = 1; i < items.length; i++) {
-					changeView |= currentView.join(Integer.parseInt(items[i]));
+					if(currentView.join(Integer.parseInt(items[i]))) {
+						sendNewView();
+					}
+					System.out.println(currentView.toString());
 				}
-				if(changeView) {
-					currentView.changeViewID();
-					changeView();
-				}
-				System.out.println(currentView.toString());
-				changeView = false;
 			}
 			else if(line.indexOf("leave") != -1) {
 				items = line.split(" ");
 				for(int i = 1; i < items.length; i++) {
-					changeView |= currentView.leave(Integer.parseInt(items[i]));
+					if(currentView.leave(Integer.parseInt(items[i]))) {
+						sendNewView();
+					}
+					System.out.println(currentView.toString());
 				}
-				if(changeView) {
-					currentView.changeViewID();
-					changeView();
-				}
-				System.out.println(currentView.toString());
-				changeView = false;
 			}
 			else {
 				System.out.println("INVALID COMMAND. PLEASE TRY AGAIN.");
@@ -67,15 +78,16 @@ public class Controller {
 			}
 		}
 	}
-	public static void changeView() throws IOException {
-		
-		ByteArrayOutputStream bStream = new ByteArrayOutputStream();
-		ObjectOutput oo = new ObjectOutputStream(bStream); 
-		oo.writeObject(currentView);
-		oo.close();
-
-		byte[] serializedMessage = bStream.toByteArray();
-		sendPacket = new DatagramPacket(serializedMessage, serializedMessage.length, IPAddress, 6789);
-		publisherSocket.send(sendPacket);	
+	public static void sendNewView(){
+		for(ObjectOutputStream obj: outputStreams) {
+			try {
+				obj.writeObject(currentView);
+				obj.flush();
+				obj.reset();
+			} catch (IOException e) {
+				System.err.println("Connection Failed");
+				System.exit(-1);
+			}
+		}
 	}
 }
