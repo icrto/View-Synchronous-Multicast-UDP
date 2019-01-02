6961a0c481f0dbebda64923e67676d36b01747d8
danielgranhao <up201406280@fe.up.pt>
2019-01-02 19:06:52 +0000

    Changed main method for testing

--- View Synchronous Multicast/src/node/Main.java
+++ View Synchronous Multicast/src/node/Main.java
@@ -5,7 +5,20 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Group g = new Group();
+		//Group g = new Group();
+		
+		VSM vsm = new VSM(1, "228.0.0.4", 6790, 0, 0, 0);
+		vsm.start();
+		
+		vsm.sendVSM("Teste");
+		vsm.sendVSM("Teste");
+		vsm.sendVSM("Teste");
+		vsm.sendVSM("Teste");
+		
+		System.out.println(vsm.recvVSM());
+		System.out.println(vsm.recvVSM());
+		System.out.println(vsm.recvVSM());
+		System.out.println(vsm.recvVSM());
 	}
 
 }
